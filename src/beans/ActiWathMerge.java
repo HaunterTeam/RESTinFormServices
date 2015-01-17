d4fed9044ef09c336d58ab92997b110619bd86f9
luca-zamboni <lucazamboni92@gmail.com>
2015-01-17 16:56:42 +0100

    forgot to add something

---
+++ src/beans/ActiWathMerge.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import getweather.Weather;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import project.models.Phrase;
+
+/**
+ *
+ * @author luca
+ */
+@XmlRootElement(name="weatherplan")
+@XmlAccessorType(XmlAccessType.PROPERTY)
+    
+public class ActiWathMerge {
+    
+    Phrase activityplan;
+    Weather weather;
+
+    public ActiWathMerge(Phrase p, Weather w) {
+        this.activityplan = p;
+        this.weather = w;
+    }
+
+    public ActiWathMerge() {
+    }
+
+    public Phrase getActivityplan() {
+        return activityplan;
+    }
+
+    public void setActivityplan(Phrase activityplan) {
+        this.activityplan = activityplan;
+    }
+
+    public Weather getWeather() {
+        return weather;
+    }
+
+    public void setWeather(Weather weather) {
+        this.weather = weather;
+    }
+
+    
+    
+    
+    
+}
