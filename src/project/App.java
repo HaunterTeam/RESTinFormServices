91444beda8c48dc8145d89cd4a71b8c4d2fb6be4
les <nanotrippolo@gmail.com>
2015-01-20 12:04:02 +0100

    started second process

--- src/project/App.java
+++ src/project/App.java
@@ -2,6 +2,7 @@ package project;
 
 import project.getflickr.FlickrService;
 import project.getflickr.Photo;
+import project.getfood.Food;
 import project.getfood.FoodService;
 import project.getphrase.PhraseService;
 
@@ -9,6 +10,10 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -37,6 +42,27 @@ public class App
         //service.getFoodNutritionValues("pizza");
         /*FlickrService service = new FlickrService();
         service.getPhotoFromTag("pizza");*/
+
+    }
+    //To redesign. This function should serve as one of the processes, it's the first try and it will be moved soon.
+    public static void firstProcess(){
+
+        //[TODO Mirko] add other food
+        List<String> foods = new ArrayList<>(Arrays.asList("pizza", "bacon", "salad","potato","roast","pasta","pasta carbonara","noodles"));
+        Random rmd = new Random();
+
+        String food = foods.get(rmd.nextInt(foods.size() - 1));
+
+        //The process will now start
+
+        FoodService service = new FoodService();
+        Food foodObject = service.getFoodNutritionValues(food);
+
+        FlickrService flickrServices = new FlickrService();
+        Photo photo = flickrServices.getPhotoFromTag(food);
+
+
+
 
     }
 
