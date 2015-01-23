ff040f27fae29ed04aab465edc74d4c03cf0949c
Roberto Zen <rob.zenn92@hotmail.it>
2015-01-23 15:26:11 +0100

    debug mode

--- src/project/utils/RequestHandler.java
+++ src/project/utils/RequestHandler.java
@@ -81,7 +81,7 @@ public class RequestHandler {
         People people = null;
         try {
 //        	URL url = new URL(Settings.LOCAL_DB_BASE_URL + Settings.LOCAL_DB_BASE_PORT + Settings.LOCAL_DB_BASE_PATH);
-        	URL url = new URL(Settings.BASE_PROTOCOL + Settings.DB_BASE_URL + Settings.DB_BASE_PORT + Settings.DB_BASE_PATH);
+        	URL url = new URL(Settings.DB_BASE_URL + Settings.DB_BASE_PORT + Settings.DB_BASE_PATH);
             QName qname = new QName(Settings.DB_BASE_ENDPOINT, Settings.DB_BASE_SERVICE);
             Service service = Service.create(url, qname);
             people = service.getPort(People.class);
